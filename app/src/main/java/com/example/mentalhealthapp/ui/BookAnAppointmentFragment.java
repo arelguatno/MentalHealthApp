ee678f9d241fefe88ed02d9e503b47b9971f3567
Paulino, Noel B <noel.b.paulino@accenture.com>
2020-08-20 22:51:17 +0800

    Date can be selected in booking

--- app/src/main/java/com/example/mentalhealthapp/ui/BookAnAppointmentFragment.java
+++ app/src/main/java/com/example/mentalhealthapp/ui/BookAnAppointmentFragment.java
@@ -1,9 +1,15 @@
 package com.example.mentalhealthapp.ui;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +18,41 @@ import androidx.fragment.app.Fragment;
 
 import com.example.mentalhealthapp.R;
 
+import java.util.Calendar;
+
 public class BookAnAppointmentFragment extends Fragment {
+
+    TextView date;
+    ImageView calendar_img;
+    DatePickerDialog.OnDateSetListener listener;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_book_an_appointment, container,false);
+        date = (TextView) v.findViewById(R.id.textView2);
+        calendar_img = (ImageView) v.findViewById(R.id.imageView3);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        date.setText(year+"/"+(month+1)+"/"+day);
+        calendar_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), listener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                datePickerDialog.show();
+            }
+        });
+
+        listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                i1 = i1 + 1;
+                date.setText(i+"/"+i1+"/"+i2);
+            }
+        };
 
         return v;
     }
