1f69edc6120bae774368de0bf6db159954e5649f
Paulino, Noel B <noel.b.paulino@accenture.com>
2020-08-21 23:21:45 +0800

    Started recycler view for appointment booking

---
+++ app/src/main/java/com/example/mentalhealthapp/java_objects/DoctorListItemModel.java
@@ -0,0 +1,10 @@
+package com.example.mentalhealthapp.java_objects;
+
+public class DoctorListItemModel {
+
+    public String photoURL;
+    public String docNme;
+    public double rating;
+    public String time;
+
+}
