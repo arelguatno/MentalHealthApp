a8110d4e5e3913ec0de608255dfe513be50cbeb5
Janelle Ann Lagatuz <jlagatuz@google.com>
2020-08-22 21:29:50 +0800

    Video Call

--- app/src/main/java/com/example/mentalhealthapp/ui/MainActivity.java
+++ app/src/main/java/com/example/mentalhealthapp/ui/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.mentalhealthapp.ui;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.fragment.app.Fragment;
 
 import com.example.mentalhealthapp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
