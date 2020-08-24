548aab50d8520cb7da3111c981cd73ac1c8ef046
Kyle Mari Torralba <ktorralba@google.com>
2020-08-24 14:59:39 +0800

    Able to save user profile data

--- app/src/main/java/com/example/mentalhealthapp/repository/UserProfileRepository.java
+++ app/src/main/java/com/example/mentalhealthapp/repository/UserProfileRepository.java
@@ -6,15 +6,21 @@ import android.net.Uri;
 import androidx.annotation.NonNull;
 
 import com.example.mentalhealthapp.java_objects.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserProfileRepository {
 
@@ -25,7 +31,7 @@ public class UserProfileRepository {
     }
 
     /* Fetches user data from Firestore */
-    public void getUserProfile(final UserProfileCallback callback) {
+    public void getUserProfile(final UserProfileCallback result) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         final UserModel profile = new UserModel();
@@ -48,21 +54,23 @@ public class UserProfileRepository {
                             profile.setEmail(document.get("email").toString());
                             profile.setToDoctor(Boolean.parseBoolean(document.get("isADoctor").toString()));
 
-                            callback.onSuccess(profile);
+                            result.onSuccess(profile);
                         }else{
-                            callback.onFailure("Problem occurred while fetching user data");
+                            result.onFailure("Problem occurred while fetching user data");
                         }
 
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        callback.onFailure("Error occurred while connecting to the database");
+                        result.onFailure("Error occurred while connecting to the database");
                     }
                 });
     }
 
-    public void saveProfilePhoto(final Uri input, final UserProfileCallback callback){
+    /* Upload profile photo to Cloud Storage and save the download url
+    to the user profile document from Firestore */
+    public void saveProfilePhoto(final Uri input, final UserProfileCallback result){
         // Getting current user's UID
         final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         // Getting the input's file extension
@@ -84,12 +92,12 @@ public class UserProfileRepository {
                             FirebaseFirestore.getInstance().collection("users")
                                     .document(uid).update("image", uri.toString());
 
-                            callback.onSuccess("Profile photo saved successfully");
+                            result.onSuccess("Profile photo saved successfully");
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception exception) {
-                            callback.onFailure("Error occurred while saving the photo");
+                            result.onFailure("Error occurred while saving the photo");
                         }
                     });
                 }
@@ -97,9 +105,71 @@ public class UserProfileRepository {
             .addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                    callback.onFailure("Error occurred while saving the photo");
+                    result.onFailure("Error occurred while saving the photo");
                 }
             });
+    }
+
+    public void savePersonalDetails(final UserModel input, final UserProfileCallback result){
+        // Getting current user object
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Prepares the given personal data in a map
+        Map<String, Object> personalDetails = new HashMap<String, Object>() {{
+            put("first_name", input.getFirst_name());
+            put("last_name", input.getLast_name());
+            put("display_name", input.getDisplay_name());
+        }};
+
+        // Updates the given fields in their corresponding document from Firestore
+        FirebaseFirestore.getInstance().collection("users")
+                .document(user.getUid()).update(personalDetails)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Turns to Firebase Auth to update the current user's display name
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(input.getDisplay_name())
+                                .build();
+                        user.updateProfile(profileUpdates);
+
+                        result.onSuccess("Successfully updated");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        result.onFailure("Error occurred while updating your personal details");
+                    }
+                });
+    }
+
+    public void saveContactDetails(final UserModel input, final UserProfileCallback result){
+        // Getting current user object
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Prepares the given personal data in a map
+        Map<String, Object> contactDetails = new HashMap<String, Object>() {{
+            put("mobile_number", input.getMobile_number());
+            put("email", input.getEmail());
+        }};
+
+        // Updates the given fields in their corresponding document from Firestore
+        FirebaseFirestore.getInstance().collection("users")
+                .document(user.getUid()).update(contactDetails)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Turns to Firebase Auth to update the current user's display name
+                        user.updateEmail(input.getEmail());
+
+                        result.onSuccess("Successfully updated");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        result.onFailure("Error occurred while update your contact details");
+                    }
+                });
     }
 
 }
